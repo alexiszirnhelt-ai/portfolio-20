dfca19008a71e298295b81ec2f983a7debddf38f
Alex.ZIRNHELT <alexis.zirnhelt@gmail.com>
2026-01-28 13:59:11 +0100

    add content header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,32 @@ import { NavLink } from "react-router-dom";
 
 function Header () {
     return (
-        <header>
-            <p>John Doe</p>
-        </header>
+        <>
+            <header>
+                <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+                    <div className="container-fluid">
+                        <NavLink className="navabr-brand">John Doe</NavLink>
+                    </div>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/Services">Services</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/Portfolio">Portfolio</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/Contact">Contact</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link text-nowrap" to="/mentions-legales">Mentions Légales</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
 
