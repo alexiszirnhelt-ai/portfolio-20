795bf526fe6fcce33a2154bba38850f90f98509d
Alex.ZIRNHELT <alexis.zirnhelt@gmail.com>
2026-01-28 17:03:54 +0100

    add hamburger Header + header.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,27 +4,35 @@ function Header () {
     return (
         <>
             <header>
-                <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+                <nav className="navbar navbar-expand-sm bg-dark navbar-dark p-2">
                     <div className="container-fluid">
-                        <NavLink className="navabr-brand">John Doe</NavLink>
+                        <NavLink className="navbar-brand" to="#">JOHN DOE</NavLink>
+                    
+
+                        {/*Bouton Hambuger*/}
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+
+                        {/*Navigation*/}
+                        <div className="collapse navbar-collapse" id="mainNavbar">
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/">HOME</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/services">SERVICES</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/portfolio">PORTFOLIO</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/contact">CONTACT</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link text-nowrap" to="/mentions-legales">MENTIONS LÉGALES</NavLink>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/Services">Services</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/Portfolio">Portfolio</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/Contact">Contact</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link text-nowrap" to="/mentions-legales">Mentions Légales</NavLink>
-                        </li>
-                    </ul>
                 </nav>
             </header>
         </>
