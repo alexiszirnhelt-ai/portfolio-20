0871b2cfa1eabfe962c55a79cdb8ab12e72c5aa3
Alex.ZIRNHELT <alexis.zirnhelt@gmail.com>
2026-01-30 11:20:29 +0100

    adjustments for responsive portfolio+services

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -19,87 +19,102 @@ function Portfolio () {
                     backgroundColor:"#0d6efd",
                 }}></div>
             </section>
+            
+            {/*Cards Container*/}
+            <div className="container py-5">
+            {/*card ligne 1*/}            
+                <div className="row g-4 justify-content-center mb-4">
+                    {/*card 1*/}
+                    <div className="col-12 col-md6 col-lg-4">
+                        <div className="card text-center h-100 hover-effect">
+                            <img src={freshfood} alt="card-img-freshfood"/>
+                            <div className="card-body">
+                                <h2 className="fw-bold">Fresh Food</h2>
+                                <p>Site de vente de produits frais en ligne</p>
+                                <a href="https://www.freshfood.fr/" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
+                            </div>
+                            <div className="card-footer">
+                                <p>Site réalisé avec PHP et MySQL</p>
+                            </div>
+                        </div>
+                    </div>
+                    {/*card 2*/}
+                    <div className="col-12 col-md6 col-lg-4">
+                        <div className="card text-center h-100 hover-effect">
+                            <img src={Akira} alt="card-img-restaurant-Akira"/>
+                            <div className="card-body">
+                                <h2 className="fw-bold">Fresh Food</h2>
+                                <p>Site de vente de produits frais en ligne</p>
+                                <a href="https://www.akirabackparis.com/fr/" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
+                            </div>
+                            <div className="card-footer">
+                                <p>Site réalisé avec WordPress</p>
+                            </div>
+                        </div>
+                    </div>
+                    {/*card 3*/}
+                    <div className="col-12 col-md6 col-lg-4">
+                        <div className="card text-center h-100 hover-effect">
+                            <img src={BienEtre} alt="card-img-BienEtre"/>
+                            <div className="card-body">
+                                <h2 className="fw-bold">Espace bien-être</h2>
+                                <p>Site de vente de produits frais en ligne</p>
+                                <a href="https://www.espace-bienetre.com/" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
+                            </div>
+                            <div className="card-footer">
+                                <p>Site réalisé avec LARAVEL</p>
+                            </div>
+                        </div>
+                    </div>
+                {/* ----- */}
+                </div>
 
-            {/*card ligne 1*/}
-            <div className="d-flex justify-content-center gap-4 mt-4">
-                {/*card 1*/}
-                <div className="card text-center hover-effect" style={{width:"400px"}}>
-                    <img src={freshfood} alt="card-img-freshfood"/>
-                    <div className="card-body">
-                        <h2 className="fw-bold">Fresh Food</h2>
-                        <p>Site de vente de produits frais en ligne</p>
-                        <a href="https://www.freshfood.fr/" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
+                {/*card ligne 2*/}
+                <div className="row g-4 justify-content-center">
+                    {/*card 4*/}
+                    <div className="col-12 col-md6 col-lg-4">
+                        <div className="card text-center h-100 hover-effect">
+                            <img src={SEOimg} alt="card-img-SEO"/>
+                            <div className="card-body">
+                                <h2 className="fw-bold">SEO</h2>
+                                <p>Amélioration du référencement d'un site e-commerce</p>
+                                <a href="https://www.seo.fr/" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
+                            </div>
+                            <div className="card-footer">
+                                <p>Utilisation des outils SEO</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="card-footer">
-                        <p>Site réalisé avec PHP et MySQL</p>
+                    {/*card 5*/}
+                    <div className="col-12 col-md6 col-lg-4">
+                        <div className="card text-center h-100 hover-effect">
+                            <img src={APIimg} alt="card-img-API"/>
+                            <div className="card-body">
+                                <h2 className="fw-bold">Création d'une API</h2>
+                                <p>Création d'une API RESTFULL publique</p>
+                                <a href="https://www.redhat.com/fr/topics/api/what-are-application-programming-interfaces" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
+                            </div>
+                            <div className="card-footer">
+                                <p>PHP - SYMPHONY</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                {/*card 2*/}
-                <div className="card text-center hover-effect" style={{width:"400px"}}>
-                    <img src={Akira} alt="card-img-restaurant-Akira"/>
-                    <div className="card-body">
-                        <h2 className="fw-bold">Fresh Food</h2>
-                        <p>Site de vente de produits frais en ligne</p>
-                        <a href="https://www.akirabackparis.com/fr/" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
+                    {/*card 6*/}
+                    <div className="col-12 col-md6 col-lg-4">
+                        <div className="card text-center h-100 hover-effect">
+                            <img src={maquette} alt="card-img-desk-pc"/>
+                            <div className="card-body">
+                                <h2 className="fw-bold">Maquette d'un site web</h2>
+                                <p>Création du prototype d'un site</p>
+                                <a href="https://www.figma.com/login" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
+                            </div>
+                            <div className="card-footer">
+                                <p>Réalisé avec FIGMA</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="card-footer">
-                        <p>Site réalisé avec WordPress</p>
-                    </div>
-                </div>
-                {/*card 3*/}
-                <div className="card text-center hover-effect" style={{width:"400px"}}>
-                    <img src={BienEtre} alt="card-img-BienEtre"/>
-                    <div className="card-body">
-                        <h2 className="fw-bold">Espace bien-être</h2>
-                        <p>Site de vente de produits frais en ligne</p>
-                        <a href="https://www.espace-bienetre.com/" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
-                    </div>
-                    <div className="card-footer">
-                        <p>Site réalisé avec LARAVEL</p>
-                    </div>
-                </div>
             {/* ----- */}
-            </div>
-
-            {/*card ligne 2*/}
-            <div className="d-flex justify-content-center gap-4 mt-4 mb-5">
-                {/*card 4*/}
-                <div className="card text-center hover-effect" style={{width:"400px"}}>
-                    <img src={SEOimg} alt="card-img-SEO"/>
-                    <div className="card-body">
-                        <h2 className="fw-bold">SEO</h2>
-                        <p>Amélioration du référencement d'un site e-commerce</p>
-                        <a href="https://www.seo.fr/" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
-                    </div>
-                    <div className="card-footer">
-                        <p>Utilisation des outils SEO</p>
-                    </div>
                 </div>
-                {/*card 5*/}
-                <div className="card text-center hover-effect" style={{width:"400px"}}>
-                    <img src={APIimg} alt="card-img-API"/>
-                    <div className="card-body">
-                        <h2 className="fw-bold">Création d'une API</h2>
-                        <p>Création d'une API RESTFULL publique</p>
-                        <a href="https://www.redhat.com/fr/topics/api/what-are-application-programming-interfaces" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
-                    </div>
-                    <div className="card-footer">
-                        <p>PHP - SYMPHONY</p>
-                    </div>
-                </div>
-                {/*card 6*/}
-                <div className="card text-center hover-effect" style={{width:"400px"}}>
-                    <img src={maquette} alt="card-img-desk-pc"/>
-                    <div className="card-body">
-                        <h2 className="fw-bold">Maquette d'un site web</h2>
-                        <p>Création du prototype d'un site</p>
-                        <a href="https://www.figma.com/login" target="_blank" className="btn-web btn btn-primary">Voir le site</a>
-                    </div>
-                    <div className="card-footer">
-                        <p>Réalisé avec FIGMA</p>
-                    </div>
-                </div>
-            {/* ----- */}
             </div>
             <style jsx>{`
                 .hover-effect:hover {
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -15,44 +15,52 @@ function Services () {
             </section>
 
             {/*3 cards*/}
-            <div className="d-flex justify-content-center gap-4 py-5">
-                {/* card ux design*/}
-                <div className="card shadow-sm hover-effect" style={{width:"400px"}}>
-                    <div className="card-content text-center">
-                        <div className="mt-5 mb-3">
-                            <i className="bi bi-brush text-primary fs-2"></i>
-                        </div>
-                        <div className="card-body">
-                            <h2 className="fw-bold">UX DESIGN</h2>
-                            <p>L'UX design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés, etc.) en plaçant l'utilisateur au centre des préocupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et agréable possible.</p>
-                        </div>
-                    </div>
-                </div>
-                {/* card Dévelopement web*/}
-                <div className="card shadow-sm hover-effect" style={{width:"400px"}}>
-                    <div className="card-content text-center">
-                        <div className="mt-5 mb-3">
-                            <i className="bi bi-code-slash text-primary fs-2"></i>
-                        </div>
-                        <div className="card-body">
-                            <h2 className="fw-bold">Développement web</h2>
-                            <p>Le développement de sites web consiste à créer des sites internet en utilisant des langes de programmation (HTML,CSS,JavaScript,PHP, etc.) et des frameworks(Bootstrap,React,Angular, etc.)</p>
+            <div className="container py-5">
+                <div className="row g-4 justify-content-center">
+                    {/* card ux design*/}
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <div className="card shadow-sm h-100 hover-effect">
+                            <div className="card-content text-center">
+                                <div className="mt-5 mb-3">
+                                    <i className="bi bi-brush text-primary fs-2"></i>
+                                </div>
+                                <div className="card-body">
+                                    <h2 className="fw-bold">UX DESIGN</h2>
+                                    <p>L'UX design est une discipline qui consiste à concevoir des produits (sites web, applications mobiles, logiciels, objets connectés, etc.) en plaçant l'utilisateur au centre des préocupations. L'objectif est de rendre l'expérience utilisateur la plus fluide et agréable possible.</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                {/* card Réferencement*/}
-                <div className="card shadow-sm hover-effect" style={{width:"400px"}}>
-                    <div className="card-content text-center">
-                        <div className="mt-5 mb-3">
-                            <i className="bi bi-search text-primary fs-2"></i>
-                        </div>
-                        <div className="card-body">
-                            <h2 className="fw-bold">Réferencement</h2>
-                            <p>Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résultats des moteurs de recherche (Google,Bing,Yahou,etc.). L'objectif est d'attirer un maximu de visiteurs qualifiés sur le site.</p>
+                    {/* card Dévelopement web*/}
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <div className="card shadow-sm h-100 hover-effect">
+                            <div className="card-content text-center">
+                                <div className="mt-5 mb-3">
+                                    <i className="bi bi-code-slash text-primary fs-2"></i>
+                                </div>
+                                <div className="card-body">
+                                    <h2 className="fw-bold">Développement web</h2>
+                                    <p>Le développement de sites web consiste à créer des sites internet en utilisant des langes de programmation (HTML,CSS,JavaScript,PHP, etc.) et des frameworks(Bootstrap,React,Angular, etc.)</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    {/* card Réferencement*/}
+                    <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card shadow-sm h-100 hover-effect">
+                        <div className="card-content text-center">
+                            <div className="mt-5 mb-3">
+                                <i className="bi bi-search text-primary fs-2"></i>
+                            </div>
+                            <div className="card-body">
+                                <h2 className="fw-bold">Réferencement</h2>
+                                <p>Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web pour le faire remonter dans les résultats des moteurs de recherche (Google,Bing,Yahou,etc.). L'objectif est d'attirer un maximu de visiteurs qualifiés sur le site.</p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
             {/* ----- */}
+                </div>
             </div>
             <style jsx>{`
                 .hover-effect:hover {
